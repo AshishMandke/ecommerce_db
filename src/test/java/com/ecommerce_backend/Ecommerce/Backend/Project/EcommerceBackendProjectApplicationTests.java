f22c112aee04d6f287fcd9db496eff03cdc10557
AshishMandke <ashishmandke.mech@gmail.com>
2025-02-19 20:49:51 +0530

    Initial commit - project structure

---
+++ src/test/java/com/ecommerce_backend/Ecommerce/Backend/Project/EcommerceBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_backend.Ecommerce.Backend.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
